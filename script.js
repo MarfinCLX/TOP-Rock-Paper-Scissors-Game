b700a343ada787fef30e02d68e949a2f0dd0b8d1
MarfinCLX <glebtkacov5@gmail.com>
2026-07-18 00:19:48 +0100

    Finished  the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello world")
+
+let getComputerChoise = "" ;
+getComputerChoise = Math.floor(Math.random() * 3) + 1;
+
+if (getComputerChoise === 1) {
+    getComputerChoise = "Rock";
+} else if (getComputerChoise === 2) {
+    getComputerChoise = "Scissors";
+} else if (getComputerChoise === 3) {
+    getComputerChoise = "Paper";
+}
+
+console.log(getComputerChoise);
